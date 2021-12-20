92363a34e222e0f04894f9d4041b5bc66db74a4f
S317 <seifenan@gmail.com>
2021-12-20 18:15:38 -0500

    modified server

--- server.js
+++ server.js
@@ -1,18 +1,68 @@
+const fs = require("fs");
+const path = require("path");
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const index = path.join(__dirname, "/public");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+let noteDb = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-// Use apiRoutes
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🌍 API server now on port ${PORT}!`);
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(index, "notes.html"));
 });
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    
+    res.json(noteDb[Number(req.params.id)]);
+});
+
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(index, "index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    
+    let newNote = req.body;
+    let noteId = (noteDb.length).toString();
+    newNote.id = noteId;
+    noteDb.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteDb));
+    console.log(newNote);
+    res.json(noteDb);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    
+    let noteId = req.params.id;
+    let newId = 0;
+    console.log(`Note ${noteId} Deleted`);
+    noteDb = noteDb.filter(thisNote => {
+        return thisNote.id != noteId;
+    }) 
+    
+    for (thisNote of noteDb) {
+        thisNote.id = newId.toString();
+        newId++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteDb));
+    res.json(noteDb);
+})
+
+app.listen(PORT, function() {
+    console.log(`🌍 Now listening on PORT ${PORT}`);
+})
